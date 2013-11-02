a50c0f3b95d8bce74364159839021d1f91b259eb
Sushant <sushant.dinesh94@gmail.com>
2013-11-02 16:02:32 +0530

    Added messagging and notification system :octocat:

--- back_end/opt/app/controllers/application_controller.rb
+++ back_end/opt/app/controllers/application_controller.rb
@@ -1,5 +1,24 @@
+require 'json'
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+
+
+
+  private
+    def makeNotification(targetUser,notification)
+        #DO THIS THROUGH A SIDEKIQ WORKER!!!!
+        #Extend the target user to be multiple users when pushed to sidekiq!
+        #notification should look like this - [<notification string>,<user_id or user_name>]
+        $Rnotification.LPUSH(targetUser,notification.to_json)
+    end
+
+    def sendMessage(targetUser,message)
+        #DO THIS THROUGH A SIDEKIQ WORKER!!!
+        #Extend the target user to be multiple users when pushed to sidekiq!
+        #notification should look like this - [<message>,<user_id or user_name>]
+        $Rmessages.LPUSH(targetUser,message.to_json)
+    end
 end
--- back_end/opt/config/routes.rb
+++ back_end/opt/config/routes.rb
@@ -1,12 +1,14 @@
 Opt::Application.routes.draw do
-  devise_for :users
+  # devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -42,7 +44,7 @@ Opt::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
