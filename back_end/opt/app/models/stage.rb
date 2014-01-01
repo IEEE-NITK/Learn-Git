94b29d7744b8db35f229d1081be43c19404fff2c
Sushant <sushant.dinesh94@gmail.com>
2014-01-02 02:27:42 +0530

    waste commit

--- back_end/opt/app/models/stage.rb
+++ back_end/opt/app/models/stage.rb
@@ -1,6 +1,7 @@
 class Stage < ActiveRecord::Base
     belongs_to :course
     serialize :hints
+    serialize :validation
 
     #validates_presence_of :course_id,:step_number,:objective,:hints,:commit_number,:validation
 
--- back_end/opt/app/models/team.rb
+++ back_end/opt/app/models/team.rb
@@ -1,5 +1,6 @@
 class Team < ActiveRecord::Base
-	serialize :ordera , :orderb
+	serialize :ordera
+             serialize :orderb
 	has_and_belongs_to_many :users
 	has_many :repos
 end
